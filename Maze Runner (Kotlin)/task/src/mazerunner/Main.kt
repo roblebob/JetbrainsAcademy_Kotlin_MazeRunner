d42c0027ed10f4a4e69105f0db89998e559ff702
roblebob <robert.koppisch@gmail.com>
2024-01-12 11:35:31 +0100

    finished 2/4

--- Maze Runner (Kotlin)/task/src/mazerunner/Main.kt
+++ Maze Runner (Kotlin)/task/src/mazerunner/Main.kt
@@ -6,9 +6,8 @@ import kotlin.math.pow
 
 fun main() {
     val maze = Maze(readln())
+    maze.mPrint()
 }
-
-
 
 
 typealias Pos = Pair<Int, Int>
@@ -20,117 +19,62 @@ fun Pos.distance(other: Pos, l: Double = 2.0) = (
     ).pow(1 / l )
 
 
-
-
-
-
-
 class Maze(val height: Int, val width: Int = height, val distance: Double = 2.0, val bound: Int = 1) {
     constructor(size: List<Int>) : this(size[0], size[1])
-    constructor(size: Pos) : this(size.row(), size.col())
     constructor(string: String) : this(string.split(" ").map { it.toInt() })
 
     // pick a random cell (but bounds)
     var next: Pos = (bound until height - bound).random() to (bound until width - bound).random()
 
-
-    private val passage = mutableSetOf<Pos>(next, )
-    private val frontier = frontierCells(next).toMutableSet()
-
-    var counter = 0
-
+    private val passage = mutableSetOf(next, )
+    private val frontier = frontierCells(next)
 
 
     init {
-        mPrint("Start")
-
-/*        // set it as passage
-        passage.add(next)
-
-        mPrint("")
-        //
-
-        frontier.addAll(frontierCells(next))
-
-        mPrint()*/
-
         while (frontier.isNotEmpty()) {
-
 
             next = frontier.random()
             frontier.remove(next)
 
-            mPrint("next chosen")
-
-            if (!isValid(next)) {
-                mPrint("invalid")
-                continue
-            }
-
-
-            passage.add(next)
-
-
-            mPrint("next added")
+            if (isValid(next)) { passage.add(next) }
 
             val closest = passage.filter { it.distance(next) == distance }.random()
-            val inBetween = ((next.row() + closest.row()) / 2) to ((next.col() + closest.col())) / 2
-            passage.add(inBetween)
+            val inBetween = ((next.row() + closest.row()) / 2) to ((next.col() + closest.col()) / 2)
+            if (isValid(inBetween)) { passage.add(inBetween) }
 
-            mPrint("inBetween added")
-
-            if (isValid(next)) {
-                frontier.addAll(frontierCells(next))
-            } else {
-                println("invalid")
-            }
-
-            mPrint("frontier extended")
-
-            next = Pos(-1, -1)  // only for visualization debugging
-            mPrint("next removed")
+            if (isValid(next)) { frontier.addAll(frontierCells(next)) }
         }
     }
 
 
-
-
-
     private fun isValid(pos: Pos): Boolean {
-
-        if (pos.row() !in 1 until height - 1) return false
+        if (pos.col() == 0          && passage.any { it.col() == 0 }) return false  // only one entrance
+        if (pos.col() == width - 1  && passage.any { it.col() == width - 1 }) return false  // only one exit
         if (pos.col() !in 0 until width) return false
-        if (pos.col() == 0          && passage.any { it.col() == 0 }) return false
-        if (pos.col() == width - 1  && passage.any { it.col() == width - 1 }) return false
+        if (pos.row() !in bound until height - bound) return false
         return true
     }
 
 
-    private fun frontierCells(currentPos: Pos, distance: Int = 2 ): Set<Pos>
-    {
+    private fun frontierCells(currentPos: Pos, distance: Int = 2 ): MutableSet<Pos> {
         val (row, col) = currentPos
-        val frontierCells = mutableSetOf<Pos>()
-        for (i in -distance..distance step (2 * distance) ) {
-            if (row + i in 1 until height - 1  &&  row + i to col !in passage) {
-                frontierCells.add(row + i to col)
-            }
-            if (col + i in -1 until width + 1  &&  row to col + i !in passage) {
-                frontierCells.add(row to col + i)
-            }
-        }
-        return frontierCells
+        return mutableSetOf(
+            row - distance to col,
+            row + distance to col,
+            row to col - distance,
+            row to col + distance
+        ).filter { it !in passage }.toMutableSet()
     }
 
 
     fun mPrint(msg: String = "") {
 
-        println("${++counter}:${msg}       XX==$next")
         for (row in 0 until height) {
             for (col in 0 until width) {
                 print(
                     when (Pos(row, col)){
-                        in setOf(next) -> NEXT_STR
-                        in frontier -> FRONTIER_STR
+                        //in setOf(next) -> NEXT_STR
+                        //in frontier -> FRONTIER_STR
                         in passage -> PATH_STR
                         else -> WALL_STR
                     }
@@ -138,16 +82,15 @@ class Maze(val height: Int, val width: Int = height, val distance: Double = 2.0,
             }
             println()
         }
-        println("\n\n")
-        if (counter % 10 == 0) readln()
-
     }
 
 
     companion object {
+        const val red = "\u001b[31m"
+        const val reset = "\u001b[0m"
         const val WALL_STR = "\u2588\u2588"  // ██
         const val PATH_STR = "  "
         const val FRONTIER_STR = "\u2592\u2592"  // ▒▒
-        const val NEXT_STR = "\u2591\u2591"  // ░░
+        const val NEXT_STR = red + "\u2591\u2591" + reset  // ░░
     }
 }
